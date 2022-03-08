275dfffa3979cb10a29c6708de4ad566a3a1ad21
Jeslyn Guo <jguo@greenwichacademy.org>
2022-03-08 17:58:18 -0500

    Initial Commit

---
+++ Content Creator/Content_CreatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Content_CreatorApp.swift
+//  Content Creator
+//
+//  Created by Jeslyn Guo '22 on 3/8/22.
+//
+
+import SwiftUI
+
+@main
+struct Content_CreatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
